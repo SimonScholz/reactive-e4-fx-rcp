49d2d0ab595776fafb32463be48f963d921ed78e
Simon Scholz <simon.scholz@vogella.com>
2019-03-06 21:56:42 +0100

    Added xvfb as service for travis ci

--- bundle/com.simonscholz.app/src/com/simonscholz/app/part/ElementsPart.java
+++ bundle/com.simonscholz.app/src/com/simonscholz/app/part/ElementsPart.java
@@ -3,12 +3,14 @@ package com.simonscholz.app.part;
 
 import javax.annotation.PostConstruct;
 
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
 public class ElementsPart {
 	
 	@PostConstruct
 	public void postConstruct(BorderPane parent) {
-		
+		Button button = new Button("Hello");
+		parent.setCenter(button);
 	}
 }
