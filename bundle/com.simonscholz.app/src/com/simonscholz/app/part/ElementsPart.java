764bbc37544b2fcf4c9076e5232bf10bf1c0d2d5
Simon Scholz <simon.scholz@vogella.com>
2019-03-06 21:42:12 +0100

    Initial commit of the project

---
+++ bundle/com.simonscholz.app/src/com/simonscholz/app/part/ElementsPart.java
@@ -0,0 +1,14 @@
+ 
+package com.simonscholz.app.part;
+
+import javax.annotation.PostConstruct;
+
+import javafx.scene.layout.BorderPane;
+
+public class ElementsPart {
+	
+	@PostConstruct
+	public void postConstruct(BorderPane parent) {
+		
+	}
+}
