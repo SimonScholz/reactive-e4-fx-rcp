9f5aabfe23592fe01869c233daa4f02e64bd1f38
Simon Scholz <simon.scholz@vogella.com>
2019-03-21 11:35:32 +0100

    Make use of @Component annotation for default theme

--- bundle/com.simonscholz.re4fx.app/src/com/simonscholz/re4fx/app/themes/DefaultTheme.java
+++ bundle/com.simonscholz.re4fx.app/src/com/simonscholz/re4fx/app/themes/DefaultTheme.java
@@ -1,9 +1,25 @@
 package com.simonscholz.re4fx.app.themes;
 
+import org.eclipse.fx.ui.services.theme.Stylesheet;
 import org.eclipse.fx.ui.theme.AbstractTheme;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
 
+@Component(name = "theme-default", service = org.eclipse.fx.ui.services.theme.Theme.class)
 public class DefaultTheme extends AbstractTheme {
 	public DefaultTheme() {
 		super("theme.default", "Default theme", DefaultTheme.class.getClassLoader().getResource("css/default.css"));
+	}
+	
+	@Reference(cardinality = ReferenceCardinality.MULTIPLE)
+	@Override
+	public void registerStylesheet(Stylesheet stylesheet) {
+		super.registerStylesheet(stylesheet);
+	}
+	
+	@Override
+	public void unregisterStylesheet(Stylesheet stylesheet) {
+		super.unregisterStylesheet(stylesheet);
 	}
 }
